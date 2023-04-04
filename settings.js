1b44f3c79453c11b57b63fbde127f34d33093d90
SLDAHAMBOT <122686667+SLDAHAMBOT@users.noreply.github.com>
2023-04-04 20:31:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
+global.ownernomer = "0714453755" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94786802371'] //ur premium number
 global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
